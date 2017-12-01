575ee8a4d69e0a4f1cc5fa91894cea8f9183110d
Intoit82 <30686909+Intoit82@users.noreply.github.com>
2017-12-01 09:09:26 +0200

    Add files via upload

---
+++ Contracts/Transferable.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.18;
+
+import "./Ownable.sol";
+
+contract Transferable is  Ownable
+{
+    bool public allowTransfers;
+    
+    event OnSetTransfer(address sender,bool newState);
+    
+    modifier canTransfer()
+    {
+        require(allowTransfers);
+        _;
+    }
+    
+    function setTransfers(bool isAllowed)
+    onlyOwner
+    public
+    returns(bool)
+    {
+        require(isAllowed != allowTransfers);
+        
+        allowTransfers = isAllowed;
+        
+        OnSetTransfer(msg.sender,allowTransfers);
+        
+        return true;
+    }
+    
+    function isTransfersAllowed()
+    public
+    returns(bool)
+    {
+        return allowTransfers;
+    }
+}
