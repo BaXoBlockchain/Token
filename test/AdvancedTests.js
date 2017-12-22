6a47aa0c95891eef717dab6c34450a5c425af631
Intoit82 <fixgasket@gmail.com>
2017-12-22 11:02:47 +0200

    Adding minting stress test

--- test/AdvancedTests.js
+++ test/AdvancedTests.js
@@ -1,8 +1,13 @@
 web3.eth.getTransactionReceiptMined = require("../utils/getTransactionReceiptMined.js");
 Promise = require("bluebird");
+Promise.allSeq = require("../utils/sequentialPromise.js");
 Promise.allNamed = require("../utils/sequentialPromiseNamed.js");
 const randomIntIn = require("../utils/randomIntIn.js");
 const toBytes32 = require("../utils/toBytes32.js");
+
+if (typeof web3.version.getNodePromise === "undefined") {
+    Promise.promisifyAll(web3.version, { suffix: "Promise" });
+}
 
 if (typeof web3.eth.getAccountsPromise === "undefined") {
     Promise.promisifyAll(web3.eth, { suffix: "Promise" });
@@ -15,6 +20,8 @@ contract('Token Miniting', function(accounts) {
 
     let ledger, token0, token1,
         devTeam, user0, user1,user2;
+
+    let isTestRPC;
         
     const mintValue = randomIntIn(500, 1000);
     const transferValue = randomIntIn(1, 100);
@@ -25,9 +32,10 @@ contract('Token Miniting', function(accounts) {
         user0 = accounts[1];
         user1 = accounts[2];
         user2 = accounts[3];
-        return web3.eth.getBalancePromise(devTeam)
-            .then(balance => assert.isAtLeast(web3.fromWei(balance).toNumber(), 10));
-            console.log("Before operation");
+        return web3.version.getNodePromise()
+        .then(node => isTestRPC = node.indexOf("TestRPC") > -1)
+        .then(() => web3.eth.getBalancePromise(devTeam))
+        .then(balance => assert.isAtLeast(web3.fromWei(balance).toNumber(), 10));
     });
 
     describe("Create Ledger", function() {
@@ -84,8 +92,51 @@ contract('Token Miniting', function(accounts) {
             })
 
          });
+
+        const count = 120;
+            it("Stress minting", function() {
+
+              if (!isTestRPC) this.skip();
+
+              //Set operator
+              return ledger.setOperator(token0.address,{from:devTeam})
+             .then(tx => {
+              assert.strictEqual(tx.logs[0].args.newOperator,token0.address,"ledger operator permission set transfers has failed");                
+              this.slow(300000);
+              const logValue = [];
+              let overallMinting =0;
+              let totalMintingCheck =0;
+              for (let i = 1; i <= count; i++) {
+              logValue.push(() => {
+                process.stderr.write("Minting interation " + i + "          " + '\r');
+                overallMinting += i;
+                return ledger.mint(user0,i, {from: devTeam});
+               });
+              }
+              return Promise.allSeq(logValue)
+              .then(txs => {
+               console.log("Starting log retrivals...");
+                Promise.allSeq(txs.map((tx,i) => () =>{
+                    //process.stderr.write("Retriving Minting interation " + i + "          " + '\r');
+                    totalMintingCheck += tx.logs[0].args.amount.toNumber();
+                    return i;
+               }))
               
+              .then(i=> { 
+                if (i < count) return; //exit if on going iteration number
+                assert.strictEqual(totalMintingCheck,overallMinting,"Invalid total miniting amount");
+                console.log("Minited amount of " , totalMintingCheck, " was successfully verfied ");
+              })
+
+              })
+            
+            });
+
+            });
+                        
+            
         });
+
            
        
     });
